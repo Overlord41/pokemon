2ea652c5be6ac3fd2135ce25e9c803ef28d4eec6
Luis <blackkndd@gmail.com>
2021-09-15 17:40:41 -0500

    Agregando datos d ela API a la base de datos 1

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,12 +4,54 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    num_order: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
-    ID: {
-
-    }
-  });
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    imgif: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    hp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+  },{timestamps: false});
 };
